a245ebc6f70bb6f189db9a3cd498105f9909917a
天使爱爱爱 <529442986@qq.com>
2021-05-07 09:06:57 +0800

    搜索配置文件

---
+++ tvmovie/hudie/sou/111.php
@@ -0,0 +1,15 @@
+HBfl":" {
+
+"搜索内容":"http://qiqiuqq.cn/appp/sousuo/222.php",
+"搜索标签":"①号引擎-②号引擎-③号引擎-④号引擎-⑤号引擎-⑥号引擎-⑦号引擎-另类搜索",
+"搜索接口1":"http://111.45sp.cc/sg/ss.php",
+"搜索接口2":"http://qiqiuqq.cn/api/18/sou.php",
+"搜索接口3":"http://qiqiuqq.cn/api/2/sou.php",
+"搜索接口4":"http://qiqiuqq.cn/api/4/sou.php",
+"搜索接口5":"http://qiqiuqq.cn/api/5/sou.php",
+"搜索接口6":"http://qiqiuqq.cn/api/6/sou.php",
+"搜索接口7":"http://qiqiuqq.cn/api/7/sou.php",
+"搜索接口8":"http://qiqiuqq.cn/api/19/sou.php",
+"分享次数":"2",
+"启动时间":"4"
+}"
