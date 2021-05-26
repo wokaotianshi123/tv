8cf1ca45db9c977f487beb0eb67f8122b5a7a026
天使爱爱爱 <529442986@qq.com>
2021-05-27 07:20:14 +0800

    更新文件 111.php

--- tvmovie/hudie/sou/111.php
+++ tvmovie/hudie/sou/111.php
@@ -2,7 +2,7 @@ HBfl":" {
 
 "搜索内容":"https://tianshiaiaiai.coding.net/p/tvmovie/d/tv/git/raw/master/tvmovie/hudie/sou/222.php",
 "搜索标签":"①号引擎-②号引擎-③号引擎-④号引擎-⑤号引擎-⑥号引擎-⑦号引擎-另类搜索",
-"搜索接口1":"http://api.xmm3.cn/api/2/sou.php",
+"搜索接口1":"http://api.xmm3.cn/api/12/sou.php",
 "搜索接口2":"http://api.xmm3.cn/api/14/sou.php",
 "搜索接口3":"http://api.xmm3.cn/api/15/sou.php",
 "搜索接口4":"http://333.45sp.cc/xrk/ss.php",
