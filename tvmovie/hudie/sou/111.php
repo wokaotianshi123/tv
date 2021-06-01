21a760254d9a7310cbeb397735800b49c245efec
天使爱爱爱 <529442986@qq.com>
2021-06-01 12:40:28 +0800

    更新文件 111.php

--- tvmovie/hudie/sou/111.php
+++ tvmovie/hudie/sou/111.php
@@ -5,7 +5,7 @@ HBfl":" {
 "搜索接口1":"http://api.xmm3.cn/api/12/sou.php",
 "搜索接口2":"http://api.xmm3.cn/api/14/sou.php",
 "搜索接口3":"http://api.xmm3.cn/api/15/sou.php",
-"搜索接口4":"http://api.xmm3.cn/api/10.php",
+"搜索接口4":"http://api.xmm3.cn/api/10/sou.php",
 "搜索接口5":"http://api.qiqiuqq.cn/api/8/sou.php",
 "搜索接口6":"http://api.xmm3.cn/api/11/sou.php",
 "搜索接口7":"http://api.xmm3.cn/api/7/sou.php",
