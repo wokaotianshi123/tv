5798323f360ad39b9da4103231693fd81b931343
天使爱爱爱 <529442986@qq.com>
2021-06-15 07:38:16 +0800

    更新文件 111.php

--- tvmovie/hudie/sou/111.php
+++ tvmovie/hudie/sou/111.php
@@ -2,14 +2,14 @@ HBfl":" {
 
 "搜索内容":"https://tianshiaiaiai.coding.net/p/tvmovie/d/tv/git/raw/master/tvmovie/hudie/sou/222.php",
 "搜索标签":"①号引擎-②号引擎-③号引擎-④号引擎-⑤号引擎-⑥号引擎-⑦号引擎-另类搜索",
-"搜索接口1":"http://api.xmm3.cn/api/12/sou.php",
-"搜索接口2":"http://api.xmm3.cn/api/14/sou.php",
-"搜索接口3":"http://api.xmm3.cn/api/15/sou.php",
-"搜索接口4":"http://api.xmm3.cn/api/10/sou.php",
-"搜索接口5":"http://api.xmm3.cn/api/1/sou.php",
-"搜索接口6":"http://api.xmm3.cn/api/11/sou.php",
-"搜索接口7":"http://api.xmm3.cn/api/7/sou.php",
-"搜索接口8":"http://api.xmm3.cn/api/2/sou.php",
+"搜索接口1":"http://vip.xmm3.cn/api/12/sou.php",
+"搜索接口2":"http://vip.xmm3.cn/api/14/sou.php",
+"搜索接口3":"http://vip.xmm3.cn/api/15/sou.php",
+"搜索接口4":"http://vip.xmm3.cn/api/10/sou.php",
+"搜索接口5":"http://vip.xmm3.cn/api/1/sou.php",
+"搜索接口6":"http://vip.xmm3.cn/api/11/sou.php",
+"搜索接口7":"http://vip.xmm3.cn/api/7/sou.php",
+"搜索接口8":"http://vip.xmm3.cn/api/2/sou.php",
 "分享次数":"0",
 "启动时间":"4"
 }"
